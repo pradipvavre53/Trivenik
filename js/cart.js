18269a14c7ef0ce98d1da78493b7f962826ac36c
pradipvavre53 <pradipvavre53@gmail.com>
2026-05-05 14:59:07 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -4,3 +4,40 @@ cart.push({name, price});
 localStorage.setItem("cart", JSON.stringify(cart));
 alert(name + " added to cart!");
 }
+
+function displayCart() {
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let container = document.getElementById("cart-items");
+let total = 0;
+
+container.innerHTML = "";
+
+cart.forEach((item, index) => {
+total += item.price;
+
+```
+container.innerHTML += `
+  <p>${item.name} - ₹${item.price}
+  <button onclick="removeItem(${index})">Remove</button></p>
+`;
+```
+
+});
+
+document.getElementById("total").innerText = "Total: ₹" + total;
+}
+
+function removeItem(index) {
+let cart = JSON.parse(localStorage.getItem("cart"));
+cart.splice(index, 1);
+localStorage.setItem("cart", JSON.stringify(cart));
+displayCart();
+}
+
+function checkout() {
+window.location.href = "checkout.html";
+}
+
+if (window.location.pathname.includes("cart.html")) {
+displayCart();
+}
