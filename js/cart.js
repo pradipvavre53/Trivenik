cc5abadf21b94e912db432a08d8fb82f77171118
pradipvavre53 <pradipvavre53@gmail.com>
2026-05-05 12:49:22 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,6 @@
-
+function addToCart(name, price) {
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+cart.push({name, price});
+localStorage.setItem("cart", JSON.stringify(cart));
+alert(name + " added to cart!");
+}
